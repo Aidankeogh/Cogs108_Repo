eb9c2b6715107289fb6cbc96c66e8f0ee56abb9a
Amanda Smith <ams075@ucsd.edu>
2019-05-26 16:10:28 -0700

    Updated dataframes

--- main.py
+++ main.py
@@ -1,109 +1,170 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
-# In[11]:
+# In[1]:
 
 
 from preprocessor import *
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
 
 
-# In[12]:
+# In[2]:
+
+
+def build_feats(projects, category):
+    
+    # Find and print most common unigrams and bigrams in category
+    grams = grams_by_category(projects, category, do_print=False)
+
+    # Map grams to unique index for easy vectorization
+    grams_to_idx = map_gram_to_idx(grams)
+
+    # Map unique index to gram to quickly convert vectorization to txt
+    idx_to_grams = [0] * len(grams_to_idx)
+
+    for gram, idx in grams_to_idx.items():
+        idx_to_grams[idx] = gram
+        
+    # Build feats + labels for model training
+    feats = []
+    labels = []
+
+    for project in projects:
+        if project['category'][0] == category or category == 'all':
+            encoding = vectorize(project, grams_to_idx)
+
+            # Label represents amt pledged
+            label = project['pledged']
+
+            feats.append(encoding)
+            labels.append(label)
+            
+    return idx_to_grams, feats, labels
+
+
+# In[3]:
+
+
+def create_model(projects, category, validate=False):
+            
+    idx_to_grams, feats, labels = build_feats(projects, category)
+            
+    # 90-10 split feats and labels; 90% training data and 10% test data
+    feats_train = feats[:int(len(feats) * .9)]
+    feats_test  = feats[int(len(feats) * .9):]
+
+    labels_train = labels[:int(len(labels) * .9)]
+    labels_test  = labels[int(len(labels) * .9):]
+    
+    model = linear_model.Ridge(alpha=1000)     # Initialize model
+    model.fit(feats_train, labels_train)       # Train model
+    
+    # If validate=True, then validate model using 10% of data
+    if validate:
+        predictions = model.predict(feats_test)
+        
+        MSE = mean_squared_error(predictions, labels_test)
+        print("MSE:", MSE)
+        
+    word_corrs = sorted(zip(idx_to_grams, model.coef_), key=lambda t: -t[1])
+        
+    return model, word_corrs
+
+
+# In[4]:
 
 
 projects = read_data()
-category = "games"
 
 
-# In[13]:
+# In[5]:
 
 
-# Choose and print out a random sample from the set
-i = random.randint(0, len(projects)) 
+all_categories = []
 
-print(projects[i]['text'])
-print()
-print(projects[i]['text_feats'])
-print()
-print(projects[i]['category'])
-print()
-print(projects[i]['pledged'], "$ / ", projects[i]['goal'], "$")
-
-
-# In[14]:
-
-
-# Find and print most common uni-, bi-, and trigrams in category
-grams = grams_by_category(projects, category, do_print=False)
-
-# Map grams to unique index for easy vectorization
-grams_to_idx = map_gram_to_idx(grams)
-
-# Map unique index to gram to quickly convert vectorization to txt
-idx_to_grams = [0] * len(grams_to_idx)
-
-for gram, idx in grams_to_idx.items():
-    idx_to_grams[idx] = gram
-
-
-# In[15]:
-
-
-# Build feats + labels for model training
-feats = []
-labels = []
-
+# Get list of all possible categories
 for project in projects:
+    for category in project['category']:
+        all_categories.append(category)
+        
+all_categories = nltk.FreqDist(all_categories)
+
+# Get top-10 categories
+top_10_categories = [category[0] for category in all_categories.most_common(10)]
+
+
+# In[21]:
+
+
+grams = {}
+coefs = []
+
+for category in top_10_categories:
+    temp = {}
     
-    if project['category'][0] == category:
-        # Project encoding indicates which of the uni-, bi-, and 
-        # trigrams in 'text_feats' are in the n-most common grams
-        # for the category
-        encoding = vectorize(project['text_feats'], grams_to_idx)
-        
-        # Label represents amt pledged
-        label = project['pledged']
-        
-        feats.append(encoding)
-        labels.append(label)
+    LR, corrs = create_model(projects,category)
+    
+    temp['grams'] = [t[0] for t in corrs]
+    temp['monetary_impact'] = [t[1] for t in corrs]
+    
+    coefs.append([category, LR.intercept_,LR.coef_[-1]])
+    
+    grams[category] = pd.DataFrame(temp)
 
 
-# In[16]:
+# In[39]:
 
 
-# 90-10 split feats and labels; 90% training data and 10% test data
-feats_train = feats[:int(len(feats) * .9)]
-feats_test  = feats[int(len(feats) * .9):]
-
-labels_train = labels[:int(len(labels) * .9)]
-labels_test  = labels[int(len(labels) * .9):]
+grams_df = pd.concat(grams, axis=1, keys=top_10_categories)
+coefs_df = pd.DataFrame.from_records(coefs, columns=['category', 'intercept', 'goal_v_raised'], index='category')
 
 
-# In[17]:
+# In[43]:
 
 
-# Train model
-LR = linear_model.Ridge(alpha=1000)
-LR.fit(feats_train, labels_train)
+grams_df.style
 
 
-# In[18]:
+# In[40]:
 
 
-print( "Expected pledge amt. if given NO project txt: %.2f" % LR.intercept_)
-print() 
+coefs_df.style
 
 
-# In[19]:
+# In[42]:
 
 
-zipped = sorted(zip(idx_to_grams, LR.coef_), key=lambda t: -t[1])
+# How to access intercept or goal_v_raised
+print(coefs_df.loc['art']['intercept'])
 
-df = pd.DataFrame(zipped, columns=["Gram", "Monetary Impact"])
-df.style
+# How to access grams
+print(grams_df['art']['grams'][0])
 
+
+# 
+# plt.figure(figsize=(20,10))
+# plt.bar(df_top['Gram'], df_top['Monetary Impact']) 
+# plt.xlabel("Gram") 
+# plt.ylabel("Monetary Impact") 
+# plt.show() 
+
+# 
+# plt.figure(figsize=(20,10))
+# plt.bar(df_bottom['Gram'], df_bottom['Monetary Impact']) 
+# plt.xlabel("Gram") 
+# plt.ylabel("Monetary Impact")
+# plt.figure(figsize=(20,10))
+# plt.show() 
+# Actual project finder. If you're confused by a word, check this out
+word = 'camera'
+category = 'games'
+for i in range(len(projects)):
+    if word in projects[i]['text_feats'] and category in projects[i]['category']:
+        print(projects[i]['text'])
+        print(projects[i]['pledged'], "$ / ", projects[i]['goal'], "$")
+        print()
